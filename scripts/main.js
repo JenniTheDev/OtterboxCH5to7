fbf480f7f44ac20ab84d92281ce6c1074b04009f
Jenni <jfelton@csu.fullerton.edu>
2020-09-16 08:25:16 -0700

    Has working next and previous buttons

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,12 @@ var ESC_KEY = 27;
 var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var TINY_EFFECT_CLASS = 'is-tiny';
 
-function setDetails(imageUrl, titleText) {
+function setDetails(imageUrl, titleText, imageOrder) {
   'use strict';
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
   detailImage.setAttribute('src', imageUrl);
+  detailImage.setAttribute('data-image-order', imageOrder);
+
   var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
   detailTitle.textContent = titleText;
 }
@@ -26,9 +28,14 @@ function titleFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-title');
 }
 
+function orderFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-order');
+}
+
 function setDetailsFromThumb(thumbnail) {
   'use strict';
-  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail), orderFromThumb(thumbnail));
 }
 
 function addThumbClickHandler(thumb) {
@@ -36,10 +43,12 @@ function addThumbClickHandler(thumb) {
   thumb.addEventListener('click', function (event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    console.log(thumb);
     showDetails();
   });
 }
 
+// gets images from the DOM - returns an array
 function getThumbnailsArray() {
   'use strict';
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
@@ -52,6 +61,7 @@ function hideDetails() {
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+// adds an event listener for the ESC key to the body 
 function addKeyPressHandler() {
   'use strict';
   document.body.addEventListener('keyup', function (event) {
@@ -63,11 +73,26 @@ function addKeyPressHandler() {
   });
 }
 
+// gets all images into an array, calls addThumbClickHandlers to all of them
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
   addKeyPressHandler();
+
+  // initialize events for buttons instead of adding onclick events 
+  var nextButton = document.querySelector('a.button.next');
+  var previousButton = document.querySelector('a.button.previous');
+
+  nextButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    next(getCurrentImagePosition());
+  });
+
+  previousButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    prev(getCurrentImagePosition());
+  });
 }
 
 function showDetails() {
@@ -80,8 +105,32 @@ function showDetails() {
   }, 50);
 }
 
+function next(position) {
+  if (Number(position) <= 4) {
+    setNewImage(Number(position) + 1);
+  }
+}
 
+function prev(position) {
+  if (Number(position) >= 2) {
+    setNewImage(Number(position) - 1);
+  }
+}
 
+function getCurrentImagePosition() {
+  var currentImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var currentPosition = currentImage.getAttribute('data-image-order');
+
+  return currentPosition;
+}
+
+// create function that will set the correct image based on parameters
+function setNewImage(position) {
+  // get DOM element that contains the image url
+  var newImage = document.querySelector('a[data-image-order="' + position + '"]');
+  // update the target image with correct data
+  setDetails(imageFromThumb(newImage), titleFromThumb(newImage), orderFromThumb(newImage));
+}
 
 /* This has to stay at the end */
 initializeEvents();
